937dbe12c357aa1ca73024eb5c9c3a2ef89b682a
Kevin Philips Tanamas <220711789@students.uajy.ac.id>
2025-11-27 10:51:18 +0700

    adding service

---
+++ src/api.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  private baseUrl = 'http://localhost:8080/api';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get(`${this.baseUrl}/users`);
+  }
+
+}
